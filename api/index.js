125b6763b73d53840ac12bfd7a7bb28103e7b456
Sameer Dey <kalantrilonda@gmail.com>
2023-05-25 11:33:02 +0530

    port changes

--- api/index.js
+++ api/index.js
@@ -27,6 +27,6 @@ app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-app.listen(8800, () => {
+app.listen(PORT, () => {
   console.log("Backend server is running!");
 });
